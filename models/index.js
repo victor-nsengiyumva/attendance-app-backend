e53eea582d05f073fb462c97e9d070920522d144
VICTOR NSENGIYUMVA <102825673+victor-nsengiyumva@users.noreply.github.com>
2023-09-10 22:51:36 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD,{
-  host: process.env.MYSQLHOST
+  host: process.env.MYSQLHOST:process.env.MYSQLPORT
   ,
   dialect: "mysql",
 

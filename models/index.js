d02f1708f12e07045060de6013ac523bb0039588
nsenji <jerrysyre@gmail.com>
2023-09-10 20:40:05 +0300

    added config file and made access to database credentials through the environment variables

--- models/index.js
+++ models/index.js
@@ -1,16 +1,17 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD,{
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+const sequelize = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD,{
+  host: process.env.MYSQLHOST
+  ,
+  dialect: "mysql",
   operatorsAliases: false,
 
   pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
+    max: 5,
+    min: 0,
+    acquire:30000,
+    idle:10000
   }
 });
 
